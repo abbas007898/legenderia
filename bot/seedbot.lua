ef5c0a83e25405e778b20a3d99252b5a59789984
abbas007898 <abbaschitsazi007898@gmail.com>
2017-03-07 23:16:23 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {9099301030},--Sudo users
+    sudo_users = {9099301030}--Sudo users
     disabled_channels = {},_id
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
